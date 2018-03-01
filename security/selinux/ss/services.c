9c1d2a496d73b4138e03a6c6e993315223b63147
Paul Moore <paul@paul-moore.com>
2018-03-01 17:38:30 -0500

    UPSTREAM: selinux: rename the {is,set}_enforcing() functions
    Rename is_enforcing() to enforcing_enabled() and
    enforcing_set() to set_enforcing().
    
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    
    (cherry picked from commit e5a5ca96a42ca7eee19cf8694377308771350950)
    Change-Id: I6063be6809663000835461787d9e43df2f6fd853
    Bug: 140252993
    Signed-off-by: Jeff Vander Stoep <jeffv@google.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -781,7 +781,7 @@ out:
 	kfree(t);
 #endif
 
-	if (!is_enforcing(state))
+	if (!enforcing_enabled(state))
 		return 0;
 	return -EPERM;
 }
@@ -1630,7 +1630,7 @@ out:
 	kfree(n);
 #endif
 
-	if (!is_enforcing(state))
+	if (!enforcing_enabled(state))
 		return 0;
 	return -EACCES;
 }
@@ -1941,7 +1941,7 @@ static inline int convert_context_handle_invalid_context(
 	char *s;
 	u32 len;
 
-	if (is_enforcing(state))
+	if (enforcing_enabled(state))
 		return -EINVAL;
 
 	if (!context_struct_to_string(policydb, context, &s, &len)) {
