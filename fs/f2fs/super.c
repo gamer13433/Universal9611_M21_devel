9fd7fa637c41ca871bfa214c2a481c2b5affaf3a
Park Ju Hyung <qkrwngud825@gmail.com>
2019-08-09 21:59:59 +0900

    f2fs: Improve naming schemes for rapid GC
    Change-Id: I77faf559002fd0c6944da2f5820d23999c50ce34
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1259,7 +1259,7 @@ static void f2fs_put_super(struct super_block *sb)
 	 * above failed with error.
 	 */
 	f2fs_destroy_stats(sbi);
-	f2fs_sbi_list_del(sbi);
+	f2fs_gc_sbi_list_del(sbi);
 
 	/* destroy f2fs internal modules */
 	f2fs_destroy_node_manager(sbi);
@@ -3577,7 +3577,7 @@ try_onemore:
 		goto free_stats;
 	}
 
-	f2fs_sbi_list_add(sbi);
+	f2fs_gc_sbi_list_add(sbi);
 
 	/* read root inode and dentry */
 	root = f2fs_iget(sb, F2FS_ROOT_INO(sbi));
@@ -3733,7 +3733,7 @@ free_node_inode:
 	iput(sbi->node_inode);
 	sbi->node_inode = NULL;
 free_stats:
-	f2fs_sbi_list_del(sbi);
+	f2fs_gc_sbi_list_del(sbi);
 	f2fs_destroy_stats(sbi);
 free_nm:
 	f2fs_destroy_node_manager(sbi);
