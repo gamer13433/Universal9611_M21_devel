1b65af711bc3fb895d4043a791456a55a6de40fd
gamer13433 <saintpi70@gmail.com>
2020-11-15 15:41:46 +0500

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-
-
-export ANDROID_MAJOR_VERSION=q
+export KBUILD_BUILD_USER=Ramen
+export KBUILD_BUILD_HOST=SUSANO
 export ARCH=arm64
+export SUBARCH=arm64
+export ANDROID_MAJOR_VERSION=q
 make exynos9610-m21dd_defconfig
-make -j32
+make -j16
