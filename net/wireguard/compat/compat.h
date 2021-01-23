dcba55d82c137a2980e4bacf9b0b6d720a017094
gamer13433 <saintpi70@gmail.com>
2021-01-23 20:45:09 +0500

    fix wireguard after 4.14.217 linux update

--- net/wireguard/compat/compat.h
+++ net/wireguard/compat/compat.h
@@ -823,11 +823,13 @@ static __always_inline void old_rcu_barrier(void)
 #define COMPAT_CANNOT_DEPRECIATE_BH_RCU
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 10) && !defined(ISRHEL8)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 10) && !defined(ISRHEL8) 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 216) > (4, 15, 0)
 static inline void skb_mark_not_on_list(struct sk_buff *skb)
 {
 	skb->next = NULL;
 }
+#endif
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 20, 0) && !defined(ISRHEL8)
