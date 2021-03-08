56933b6afdf2fcdf364eefa336dec4cf393a0d15
gamer13433 <saintpi70@gmail.com>
2021-03-08 21:03:55 +0500

    tree: giga-clean

--- scripts/checksyscalls.sh
+++ scripts/checksyscalls.sh
@@ -211,5 +211,5 @@ syscall_list() {
 	done
 }
 
-(ignore_list && syscall_list $(dirname $0)/../arch/x86/entry/syscalls/syscall_32.tbl) | \
+(ignore_list && syscall_list $(dirname $0)/../usr/x86/syscall_32.tbl) | \
 $* -E -x c - > /dev/null
