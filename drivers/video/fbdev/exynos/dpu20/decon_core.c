89807e66ba191f51e1355146997dc9ed948aeada
gamer13433 <saintpi70@gmail.com>
2021-04-07 23:42:09 +0500

    decon: fix build witch tracing disabled.

--- drivers/video/fbdev/exynos/dpu20/decon_core.c
+++ drivers/video/fbdev/exynos/dpu20/decon_core.c
@@ -106,7 +106,7 @@ void tracing_mark_write(struct decon_device *decon, char id, char *str1, int val
 		decon_err("%s:argument fail\n", __func__);
 		return;
 	}
-	trace_puts(buf);
+	trace_printk(buf);
 
 }
 
