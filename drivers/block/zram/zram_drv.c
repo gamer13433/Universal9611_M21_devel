2cb5df94b3c5f29ff98f6ff83e0cdb0cfdb236dd
LibXZR <xzr467706992@163.com>
2021-04-24 23:18:01 +0800

    drivers: zram_drv: Expose zram_compact() for zram0
    * Automatic compaction needs this

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -80,6 +80,8 @@ module_param(wb_start_mins, int, 0644);
 
 static struct work_struct zram_wb_fb_worker;
 
+static struct zram *zram0;
+
 static void zram_free_page(struct zram *zram, size_t index);
 static int zram_bvec_read(struct zram *zram, struct bio_vec *bvec,
 				u32 index, int offset, struct bio *bio);
@@ -1025,18 +1027,26 @@ static ssize_t use_dedup_store(struct device *dev,
 
 static ssize_t compact_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
-{
-	struct zram *zram = dev_to_zram(dev);
 
-	down_read(&zram->init_lock);
-	if (!init_done(zram)) {
-		up_read(&zram->init_lock);
-		return -EINVAL;
+void zram_compact(void)
+{
+	if (!zram0)
+		return;
+
+	down_read(&zram0->init_lock);
+	if (!init_done(zram0)) {
+		up_read(&zram0->init_lock);
+		return;
 	}
 
-	zs_compact(zram->mem_pool);
-	up_read(&zram->init_lock);
+	zs_compact(zram0->mem_pool);
+	up_read(&zram0->init_lock);
+}
 
+static ssize_t compact_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t len)
+{
+	zram_compact();
 	return len;
 }
 
@@ -2237,6 +2247,7 @@ static int zram_add(void)
 	strlcpy(zram->compressor, default_compressor, sizeof(zram->compressor));
 
 	zram_debugfs_register(zram);
+	zram0 = zram;
 	pr_info("Added device: %s\n", zram->disk->disk_name);
 	return device_id;
 
@@ -2279,6 +2290,7 @@ static int zram_remove(struct zram *zram)
 	del_gendisk(zram->disk);
 	blk_cleanup_queue(zram->disk->queue);
 	put_disk(zram->disk);
+	zram0 = NULL;
 	kfree(zram);
 	return 0;
 }
